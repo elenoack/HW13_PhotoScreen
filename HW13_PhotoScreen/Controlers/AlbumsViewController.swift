53fee6a88a85ca6693cfb0f89369b8f23ccaf13a
elenoack <elenanoack@mail.ru>
2022-06-06 16:50:51 +0300

    add_CollectionView

--- HW13_PhotoScreen/Controlers/AlbumsViewController.swift
+++ HW13_PhotoScreen/Controlers/AlbumsViewController.swift
@@ -7,18 +7,88 @@
 
 import UIKit
 
-class AlbumsViewController: UIViewController {
+enum Sections: Int {
+    
+    case first = 0
+    case second = 1
+    case third = 2
+    case fourth = 3
+}
 
+class AlbumsViewController: UIViewController, UICollectionViewDelegate {
+    // MARK: - Properties
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
+        return collectionView
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        view.addSubview(collectionView)
     }
+}
 
+// MARK: - Private
+
+private extension AlbumsViewController {
+    
     func setupView() {
         view.backgroundColor = .white
-        title = "Для Вас"
+        title = "Альбомы"
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
+
+// MARK: - UICollectionViewLayout
+
+private extension AlbumsViewController {
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+                                                            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            guard let sectionLayout = Sections(rawValue: sectionIndex) else { return nil }
+            
+            switch sectionLayout {
+            case .first:
+                return nil
+            case .second:
+                return nil
+            case .third:
+                return nil
+            case .fourth:
+                return nil
+            default:
+                return nil
+            }
+        }
+        return layout
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension AlbumsViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+         return 4
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return 3
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+              return cell
+         }
+     }
 
