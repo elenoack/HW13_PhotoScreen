241cfb981f5e09e06c00cbab770937d58fb55940
elenoack <elenanoack@mail.ru>
2022-06-07 09:43:59 +0300

    add_extensions

---
+++ HW13_PhotoScreen/Extensions/UInt+Extensions.swift.swift
@@ -0,0 +1,15 @@
+//
+//  UInt+Extensions.swift.swift
+//  HW13_PhotoScreen
+//
+//  Created by mac on 06.06.22.
+//
+
+import Foundation
+
+extension UInt {
+    
+    var formattedWithSeparator: String {
+        return Formatter.withSeparator.string(for: self) ?? ""
+    }
+} 
