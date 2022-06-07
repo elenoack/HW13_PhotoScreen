8033e974572d1401a7b4b7939956ec78914c3703
elenoack <elenanoack@mail.ru>
2022-06-07 10:06:50 +0300

    add_model

---
+++ HW13_PhotoScreen/Headers/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  HW13_PhotoScreen
+//
+//  Created by mac on 07.06.22.
+//
+
+import UIKit
+
+// MARK: - Item
+
+struct Item {
+    let text: String
+    let image: UIImage?
+    let number: UInt
+}
